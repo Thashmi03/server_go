bc00b5ce1ddfa9e9fffe19d58da297507f96e300
Thashmi03 <thashmigaa.cs20@bitsathy.ac.in>
2023-08-11 14:43:12 +0530

    article

--- article/main.go
+++ article/main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-//mapping go lang to json
+// mapping go lang to json
 // caps at start
 type Article struct {
 	Id      string `json:"Id"`
@@ -19,38 +19,29 @@ type Article struct {
 }
 
 func main() {
-
-	http.HandleFunc("/", handleHello)
-	http.HandleFunc("/contact/", handleContact)
 	http.HandleFunc("/article/", handleArticle)
+	http.HandleFunc("/article1/", getArticles)
 	fmt.Println("server running on port:2000")
 	log.Fatal(http.ListenAndServe(":2000", nil))
 
 }
 
-// parameters are response and pointer ref request
-func handleHello(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.Method)
-	fmt.Println(req.URL)
-	fmt.Println(req.URL.Path)
-	if req.URL.Path != "/" {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "<h1>404 error:Page not Found<h1>")
-	} else {
-		fmt.Fprint(w, "<h1>welcome to http</h1>")
-	}
-
-}
 func handleArticle(w http.ResponseWriter, req *http.Request) {
+	// if req.URL.Path != "/" {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	fmt.Fprintf(w, "<h1>404 error:Page not Found<h1>")
+	// } else {
+	// 	fmt.Fprint(w, "<h1>welcome to http</h1>")
+	// }
 	if req.Method == "POST" {
 		reqBody, _ := io.ReadAll(req.Body)
 		var post Article
 		// unmarshal converting json object into go lang object
 		// rev is called marshaling
-		
+
 		err := json.Unmarshal(reqBody, &post)
 		post.Author = "Thash"
-		
+
 		// newData,err2:=json.Marshal(post)
 		if err != nil {
 			fmt.Println("got error")
@@ -63,24 +54,35 @@ func handleArticle(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(w, "unable to process your request")
 	}
 }
-func handleContact(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.Method)
-	fmt.Println(req.URL)
-	fmt.Println(req.URL.Path)
-	fmt.Fprint(w, "<h1>email:thashmigaa03@gmail.com</h1>")
 
+func getArticles(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		reqBody, _ := io.ReadAll(req.Body)
+		var articles [ ]Article
+		err := json.Unmarshal(reqBody, &articles)
+		if err != nil {
+			fmt.Println("got error")
+			fmt.Fprintf(w, err.Error())
+		} else {
+			fmt.Println(articles)
+			//appeding the data here
+			articles = append(articles, Article{Id: "3", Title: "Whereabouts", Content: "book", Summary: "good book", Author: "Jhumpa Lahiri"})
+			json.NewEncoder(w).Encode(articles) // sending json structure
+		}
+	}
 }
 
+// json file
 /*"Article":[{
-    "Id":"1",
-    "Title":"harry potter",
-    "Content":"book",
-    "Summary":"good book",
-    "Author":"J K Rowling"},
-    {
-    "Id":"2",
-    "Title":"wings of fire",
-    "Content":"book",
-    "Summary":"good book",
-    "Author":"A P J"
-    }]*/
+  "Id":"1",
+  "Title":"harry potter",
+  "Content":"book",
+  "Summary":"good book",
+  "Author":"J K Rowling"},
+  {
+  "Id":"2",
+  "Title":"wings of fire",
+  "Content":"book",
+  "Summary":"good book",
+  "Author":"A P J"
+  }]*/
